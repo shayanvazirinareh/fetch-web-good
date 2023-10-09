fe6f07206dd7c9a88a56215ceb8f84eafa473bd4
SHAYAN <135424349+shayanvazirinareh@users.noreply.github.com>
2023-10-09 14:40:35 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let BtnHumber=document.querySelector('.menu-humberger');
+let body=document.querySelector('.body');
+let ulHeader=document.querySelector('.ul-header');
+let slider=document.querySelector('.slider');
+let flag=false;
+let searchbtn=document.querySelector('#inputtext');
+let htmlcunt=document.querySelectorAll('.cunter-ziro')
+BtnHumber.addEventListener('click',()=>{
+    if(!flag){
+        flag=true;
+        slider.style.right='49%';
+        slider.style.transition='1s';
+        ulHeader.style.right='3%';
+        ulHeader.style.transition='1.1s';
+    }else{
+        flag=false;
+        slider.style.left='3%';
+        ulHeader.style.left='52%';
+    }
+
+})
+fetch('https://fakestoreapi.com/products/')
+.then((res)=>{
+    return res.json();
+})
+.then((complate)=>{
+let save='';
+complate.forEach((data,index)=>{
+save+=`<div class='product-fetch'>
+        <h4 class="title">${complate[index].title}</h4>
+        <img class="img" width='40%' src=${complate[index].image}>
+        <button  class='price'>${complate[index].price}</button>
+        <hr>
+        </div>
+`
+})
+document.querySelector(".product").innerHTML=save;
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+funcsearch=()=>{
+    let searching=document.querySelector('#inputtext');
+    let filter=searching.value.toLowerCase();
+    let listitem=document.querySelectorAll('.product-fetch');
+    listitem.forEach(item=>{
+        let text=item.textContent;
+        if(text.toLowerCase().includes(filter.toLowerCase())){
+           item.style.display=''
+        }else{
+            item.style.display='none'
+        }
+    })
+}
+
+htmlcunt.forEach(element=> {
+    setInterval(()=>{
+        if(element.innerHTML==220){
+    clearInterval()
+        }else{
+    
+            element.innerHTML++
+        }
+    },50)
+        
+});
+searchbtn.addEventListener('input',funcsearch)
